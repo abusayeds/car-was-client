b3a4eba1870a31a27abaae10f5d9af556faf501e
abusayeds <abusayedstudent855@gmail.com>
2024-09-06 19:08:05 +0600

    server connect

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery,  } from "@reduxjs/toolkit/query/react";
 import { RootState } from "../store";
 const baseQuery = fetchBaseQuery({
-    baseUrl: "http://localhost:5000/api/",
+    baseUrl: "https://car-wash-server-omega.vercel.app/",
     credentials: "include",
     prepareHeaders: (headers, { getState }) => {
       const token = (getState() as RootState).UserDetails.token;
